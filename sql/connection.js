9c0647148af4d23bf11694395c7742a77765eeb0
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-12-06 16:32:46 -0500

    env vars

--- sql/connection.js
+++ sql/connection.js
@@ -1,16 +1,11 @@
 const mysql = require('mysql2');
 
-let USER = 'ajeanlau_rent';
-let DATABASE = 'ajeanlau_quiz';
-let PASSWORD = 'throwaway1234';
-let HOST = '192.185.94.20';
-
 // initialize reusable sql connection
 const connection = mysql.createPool({
-    host: HOST,
-    user: USER,
-    password: PASSWORD,
-    database: DATABASE
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 module.exports = connection;
